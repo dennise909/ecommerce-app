ac19e8e8182949516122f6d4a0a01c1a38e55715
dennise909 <dennise909@hotmail.com>
2022-09-19 17:08:37 +0200

    changed keyframes

--- components/loading-status.tsx
+++ components/loading-status.tsx
@@ -6,24 +6,24 @@ type CirclesizeProps = {
   spinnercolor: string;
 };
 
+const spinner2 = keyframes({
+  to: {
+    transform: "rotate(360deg)",
+  },
+});
+
 const Circlesize = styled.div<CirclesizeProps>`
   //https://stackoverflow.com/questions/61416564/emotion-js-and-typescript-problems-when-passing-props-to-styled
   position: absolute;
-  width: ${({ size }: CirclesizeProps) => size};
-  height: ${({ size }: CirclesizeProps) => size};
-  top: calc(50% - ${({ size }: CirclesizeProps) => size} / 2);
-  left: calc(50% - ${({ size }: CirclesizeProps) => size} / 2);
+  width: ${(props) => props.size};
+  height: ${(props) => props.size};
+  top: calc(50% - ${(props) => props.size} / 2);
+  left: calc(50% - ${(props) => props.size} / 2);
   border: 4px solid transparent;
-  border-left: 4px solid ${({ spinnercolor }: CirclesizeProps) => spinnercolor};
-  border-right: 4px solid ${({ spinnercolor }: CirclesizeProps) => spinnercolor};
+  border-left: 4px solid ${(props) => props.spinnercolor};
+  border-right: 4px solid ${(props) => props.spinnercolor};
   border-radius: 50%;
-  animation: spinner2 650ms linear infinite;
-
-  @keyframes spinner2 {
-    to {
-      transform: rotate(360deg);
-    }
-  }
+  animation: ${spinner2} 650ms linear infinite;
 `;
 
 const styles = {
@@ -40,7 +40,6 @@ const styles = {
     },
   }),
   spinner: css`
-    --animation-duration: 650ms;
     position: relative;
     width: 75px;
     height: 75px;
@@ -51,8 +50,8 @@ const LoadingStatus = () => {
     <div className={styles.container}>
       <div className={styles.spinner}>
         <Circlesize size="75px" spinnercolor="#eedd99"></Circlesize>
-        <Circlesize size="50px" spinnercolor="#eec290"></Circlesize>
-        <Circlesize size="35px" spinnercolor="#eeaa88"></Circlesize>
+        <Circlesize size="60px" spinnercolor="#eec290"></Circlesize>
+        <Circlesize size="45px" spinnercolor="#eeaa88"></Circlesize>
       </div>
     </div>
   );
